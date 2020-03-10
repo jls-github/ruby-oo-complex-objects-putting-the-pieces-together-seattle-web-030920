11b85b4ddf61646e9fa0128093c3efc998e66d74
John Souza <souzajohnlee@gmail.com>
2020-03-10 21:38:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,12 @@
 class Book
   
-  attr_accessor :title
+  attr_accessor :title, :author, :page_count, :genre
   
   def initialize(title)
     @title = title
+  end
+  
+  def turn_page
   end
   
 end
